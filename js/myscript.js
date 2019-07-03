3be54d31be5ccde48bb16816edaa3588bde6b9b5
Joubert Joe Vieira Lellis <joubert.lellis@gmail.com>
2019-07-03 00:09:52 -0300

    Linha finalizada

--- js/myscript.js
+++ js/myscript.js
@@ -1,21 +1,54 @@
 function linha() {
     // F = K * (|Q1| * |Q2|) / Distancia^2
     var d;
-    var µ = Math.pow(10, -6);
-    var n = Math.pow(10, -9);
-
+    
     var d1 = parseFloat($("#posicao01").text().trim());
     var d2 = parseFloat($("#posicao02").text().trim());
-    d = d1 - d2;
+    var d3 = parseFloat($("#posicao03").text().trim());
+    
+    var q1 = parseFloat($("#carga01").text().trim());
+    var q2 = parseFloat($("#carga02").text().trim());
+    var q3 = parseFloat($("#carga03").text().trim());
+
+
+    if ((!isNaN(d1)) && (!isNaN(d2))&&(!isNaN(d3)) && (!isNaN(q1)) && (!isNaN(q2)) && (!isNaN(q3))) {
+
+        var f1 = forcaResultante(d1, d2, q1, q2);
+        var f2 = forcaResultante(d2, d3, q2, q3);
+
+        var f = f1 + f2;
+        console.log("f = " + f);
+
+
+
+        if (f == Infinity)   {
+            $('#forca').html("F = <i class='fas fa-infinity'></i>");
+        }else{
+            $('#forca').html("F = " + f);
+        }
+
+    } else if ((!isNaN(d1)) && (!isNaN(d2)) && (!isNaN(q1)) && (!isNaN(q2))) {
+
+        var f = forcaResultante(d1, d2, d3, q1, q2, q3);
+
+        $('#forca').html("F = " + f);
+
+        if (f == Infinity) {
+            $('#forca').html("F = <i class='fas fa-infinity'></i>");
+        }
+    }
+}
+
+function forcaResultante(d1, d2, q1, q2) {
+    var µ = Math.pow(10, -6);
+    var n = Math.pow(10, -9);
+    var d = d1 - d2;
     if (d < 0) {
         d = d * (-1);
     }
 
     var f;
     var k;
-    var q1 = parseFloat($("#carga01").text().trim());
-    var q2 = parseFloat($("#carga02").text().trim());
-
 
     if ($('#micro1').is(':checked')) {
         q1 = q1 * µ;
@@ -30,14 +63,6 @@ function linha() {
     if ($('#nano2').is(':checked')) {
         q2 = q2 * n;
     }
-
-    if ($('#micro3').is(':checked')) {
-        q3 = q3 * µ;
-    }
-    if ($('#nano3').is(':checked')) {
-        q3 = q3 * n;
-    }
-
 
 
     if (q1 < 0) {
@@ -59,12 +84,10 @@ function linha() {
     console.log("Distancia01: " + d1);
     console.log("Distancia02: " + d2);
     console.log("Constante: " + k);
-    $('#forca').html("F = " + f);
-    
-    if (f = Infinity){
-        $('#forca').html("F = <i class='fas fa-infinity'></i>");
-    }
 
-    
+
+    return f;
+
+
 
 }
